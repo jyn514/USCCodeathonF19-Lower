9fd063eb3a2a3b77ba1e621319b2eb4b75cc0eb4
Goldenlion5648 <32916571+Goldenlion5648@users.noreply.github.com>
2019-10-16 15:48:39 -0400

    added java solution

---
+++ TruthTable/truthTableSol.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class truthTableSol {
+    public static void main(String args[]) {
+        Scanner keyboard = new Scanner(System.in);
+        int numVariables = keyboard.nextInt();
+        int rowNum = keyboard.nextInt();
+
+        String binaryString = Integer.toBinaryString(rowNum);
+
+        binaryString = binaryString.replaceAll("1", "T ");
+        binaryString = binaryString.replaceAll("0", "F ");
+
+        System.out.println(binaryString);
+
+        keyboard.close();
+    }
+}
